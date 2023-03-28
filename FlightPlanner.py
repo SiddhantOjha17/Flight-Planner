92a7c94f712d1920552dba6f385f5d18d56bbe85
SiddhantOjha17 <96463139+SiddhantOjha17@users.noreply.github.com>
2023-03-28 20:31:35 +0530

    Update FlightPlanner.py

--- FlightPlanner.py
+++ FlightPlanner.py
@@ -20,7 +20,11 @@ def create_flight_graph(df):
         distance = row['Flight_Distance']
         airline = row['Airline']
         flight_number = row['Flight_Number']
-        graph[src][dest] = {'distance': distance, 'airline': airline, 'flight_number': flight_number, 'departure_time': dep_time, 'arrival_time': arr_time}
+        # Check if flights have already been added between these airports
+        if dest in graph[src]:
+            graph[src][dest].append({'distance': distance, 'airline': airline, 'flight_number': flight_number, 'departure_time': dep_time, 'arrival_time': arr_time})
+        else:
+            graph[src][dest] = [{'distance': distance, 'airline': airline, 'flight_number': flight_number, 'departure_time': dep_time, 'arrival_time': arr_time}]
     return graph
 
 def calculate_shortest_path(graph, start, end, flight_date):
@@ -35,45 +39,47 @@ def calculate_shortest_path(graph, start, end, flight_date):
             visited.add(node)
             path = path + [node]
             if node == end:
-                edges = []
                 total_duration = 0
                 for i in range(len(path)-1):
                     src, dest = path[i], path[i+1]
-                    edge_data = graph[src][dest]
-                    departure_time = datetime.datetime.combine(flight_date, datetime.datetime.strptime(str(edge_data['departure_time']), '%H%M').time())
-                    arrival_time = datetime.datetime.combine(flight_date, datetime.datetime.strptime(str(edge_data['arrival_time']), '%H%M').time())
-                    duration = arrival_time - departure_time
-                    total_duration += duration.seconds // 60
-                    edges.append((src, dest, {'airline': edge_data['airline'], 'flight_number': edge_data['flight_number'], 'duration': duration}))
+                    flight = graph[src][dest][0]
+                    dep_time_str = f"{str(flight['departure_time']):0>4}"
+                    arr_time_str = f"{str(flight['arrival_time']):0>4}"
+                    departure_time = datetime.datetime.combine(flight_date, datetime.datetime.strptime(dep_time_str, '%H%M').time())
+                    arrival_time = datetime.datetime.combine(flight_date, datetime.datetime.strptime(arr_time_str, '%H%M').time())
+                    total_duration += (arrival_time - departure_time).seconds // 60
                 if total_duration < shortest_duration:
                     shortest_duration = total_duration
-                    shortest_path = {'path': path, 'duration': total_duration, 'edges': edges}
-            for neighbor, edge_data in graph[node].items():
-                heappush(heap, (cost + edge_data['distance'], neighbor, path))
-    return shortest_path
-
+                    shortest_path = [(path[i], path[i+1], graph[path[i]][path[i+1]][0]) for i in range(len(path)-1)]
+            for neighbor, flights in graph[node].items():
+                for flight in flights:
+                    if flight['arrival_time'] and cost + flight['distance'] < shortest_duration:
+                        heappush(heap, (cost + flight['distance'], neighbor, path))
+    return shortest_path, shortest_duration
 
 # Load flight data
 df = load_data()
-
 # Create flight graph
 graph = create_flight_graph(df)
+st.title('Flight Planner :airplane:')
+# Create dropdown for selecting start and end airports
+start_airport = st.selectbox('Select the starting airport', df['Origin_Airport'].unique())
+end_airport = st.selectbox('Select the destination airport', df['Destination_Airport'].unique())
 
-# Define Streamlit app
-st.title('Flight Planner')
+# Create date picker for selecting flight date
+# flight_date = st.date_input('Select the flight date')
+flight_date ='2023-03-15'
+flight_date = datetime.datetime.strptime(flight_date, '%Y-%m-%d')
 
-start_airport = st.selectbox("Select the starting airport", df['Origin_Airport'].unique())
-end_airport = st.selectbox("Select the destination airport", df['Destination_Airport'].unique())
-flight_date = st.date_input('Select flight date:')
+
+# Calculate shortest path and display results
 if st.button('Find Shortest Path'):
-    result = calculate_shortest_path(graph, start_airport, end_airport, flight_date)
-    if result['duration'] == 0:
-        st.write('No flight path found')
+    shortest_path, shortest_duration = calculate_shortest_path(graph, start_airport, end_airport, flight_date)
+    if shortest_duration == float('inf'):
+        st.write(f'There is no path from {start_airport} to {end_airport}')
     else:
-        st.write('Total duration:', result['duration'], 'minutes')
-        st.write('Path:')
-        for i, edge in enumerate(result['edges']):
-            st.write(f"{i+1}. Fly {edge[2]['airline']} flight {edge[2]['flight_number']} from {edge[0]}")
-
-
+        st.write(f'The shortest path from ',start_airport, ' to ', end_airport ,' :')
+        path_str = " --> ".join([f" Flight {edge[0]} ({edge[2]['airline']} {edge[2]['flight_number']}) to {edge[1]}" for edge in shortest_path])
+        st.write(f'Path: ', path_str)
+        st.write(f'Duration: ', shortest_duration , ' minutes')
 
